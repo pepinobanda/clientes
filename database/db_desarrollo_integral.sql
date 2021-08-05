8021c1b46f4ffc8f33222d32a629e5e68c88394b
Emmanuel Banda <crissebs24@gmail.com>
2021-08-05 07:31:44 -0500

    Se adjunta el script de la base de datos utilizada.

---
+++ database/db_desarrollo_integral.sql
@@ -0,0 +1,275 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-08-2021 a las 14:16:51
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_desarrollo_integral`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `cveCategoria` smallint(6) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cveCliente` smallint(6) NOT NULL,
+  `nombre` varchar(350) NOT NULL,
+  `apellidos` varchar(450) NOT NULL,
+  `tipoCliente` smallint(6) NOT NULL,
+  `cveUsuarioFK` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`cveCliente`, `nombre`, `apellidos`, `tipoCliente`, `cveUsuarioFK`) VALUES
+(12, 'Emmanuelp', 'BandaS', 1, 1),
+(15, 'Alan', 'Renteria', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `cveDetalleVenta` smallint(6) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precioProducto` float NOT NULL,
+  `cveProducto` smallint(6) NOT NULL,
+  `cveVenta` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `cveProducto` smallint(6) NOT NULL,
+  `descripcion` varchar(350) NOT NULL,
+  `precio` float NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `cveCategoria` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `cveRol` smallint(6) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `clave` varchar(45) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`cveRol`, `descripcion`, `clave`, `activo`) VALUES
+(1, 'Cliente frecuente', 'clientef', 1),
+(2, 'Cliente enojón', 'clientee', 1),
+(3, 'Cliente ratero', 'clienter', 1),
+(4, 'Cliente carismático', 'clientec', 1),
+(5, 'Cliente afiliado', 'clienteaf', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `cveUsuario` smallint(6) NOT NULL,
+  `nombre` varchar(350) NOT NULL,
+  `apellidos` varchar(450) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(350) NOT NULL,
+  `fechaRegistro` datetime NOT NULL,
+  `cveRol` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`cveUsuario`, `nombre`, `apellidos`, `username`, `password`, `fechaRegistro`, `cveRol`) VALUES
+(1, 'Administrador', 'Admin', 'admin@gmail.com', '$2a$10$Lw9Pz55IUuiWvAMkqmz.3uNXfztIXuukIpq3cA/O0eWh3aM0rI14a', '2021-07-01 10:47:55', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `cveVenta` smallint(6) NOT NULL,
+  `totalVenta` float NOT NULL,
+  `fechaVenta` datetime NOT NULL,
+  `cveUsuario` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`cveCategoria`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cveCliente`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`cveDetalleVenta`),
+  ADD KEY `cveProducto` (`cveProducto`),
+  ADD KEY `cveVenta` (`cveVenta`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`cveProducto`),
+  ADD KEY `cveCategoria` (`cveCategoria`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`cveRol`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`cveUsuario`),
+  ADD KEY `cveRol` (`cveRol`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`cveVenta`),
+  ADD KEY `cveUsuario` (`cveUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `cveCategoria` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `cveCliente` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `cveDetalleVenta` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `cveProducto` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `cveRol` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `cveUsuario` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `cveVenta` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`cveProducto`) REFERENCES `producto` (`cveProducto`),
+  ADD CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`cveVenta`) REFERENCES `venta` (`cveVenta`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`cveCategoria`) REFERENCES `categoria` (`cveCategoria`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`cveRol`) REFERENCES `rol` (`cveRol`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`cveUsuario`) REFERENCES `usuario` (`cveUsuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
